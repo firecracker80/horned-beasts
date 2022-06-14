de449fc8c8082fcc42202dc5115fa3d5bea4ce7d
Yajahira <yari@yvelazquez.com>
2022-06-14 16:49:26 -0400

    added img

--- src/Main.js
+++ src/Main.js
@@ -7,12 +7,14 @@ class Main extends React.Component{
       <>
         <HornedBeast 
         title="Triceratops"
-        imageUrl="https://www.pexels.com/photo/styracosaurus-in-a-jungle-12365157/" alt="Triceratops"
+        imageUrl="https://www.pexels.com/photo/styracosaurus-in-a-jungle-12365157/" 
+        alt="Triceratops"
         description="Prehistoric horned herbivore."/>
-        {/* <HornedBeast 
+        <HornedBeast 
         title="Mammoth"
-        imageUrl="https://www.istockphoto.com/photo/mammoth-in-autumn-gm1133041273-300608514?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fprehistoric-animals&utm_term=prehistoric%20animals%3A%3Asearch-explore-top-affiliate-outside-feed-x-v2%3Acontrol" alt="Mammoth"
-        description="Prehistoric horned beast in its natural environment."/> */}
+        imageUrl="https://www.istockphoto.com/photo/mammoth-in-autumn-gm1133041273-300608514" 
+        alt="Mammoth"
+        description="Prehistoric horned beast in its natural environment."/>
       </>
     )
   }
