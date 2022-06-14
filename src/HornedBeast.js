0242ea934c44c642213c3e89623bf375da7cec33
Yajahira <yari@yvelazquez.com>
2022-06-13 21:00:26 -0400

    added images

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import pic from './triceratops.jpg';
 class HornedBeast extends React.Component{
   render(){
     return(
       <article>
         <header><h2>{this.props.title}</h2></header>
-        {/* <img 
-      src="https://ibb.co/YLPyRRg"
-      alt="Triceratops"
-      /> */}
+        <img style = {{width: 200, height: 300}}
+          src={pic}
+          alt="Triceratops"
+      />
       <p>Prehistoric horned beasts in their natural envionment.</p>
       </article>
     )
